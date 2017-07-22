4ec9cfcea971edceb7ccd9c83aa988960d90c5d1
mopao <franckyves78@yahoo.fr>
2017-07-22 13:23:49 -0400

    adding of empty method in a item's controller

--- Symphony/src/VanoFashion/EShoppingBundle/Controller/ItemController.php
+++ Symphony/src/VanoFashion/EShoppingBundle/Controller/ItemController.php
@@ -3,11 +3,66 @@
 namespace VanoFashion\EShoppingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ItemController extends Controller
-{
-    public function indexAction()
+{   
+
+	// return home page
+    public function indexAction(Request $request)
     {
+    	// On définit une nouvelle valeur pour cette variable user_id
+
+        $request->getSession()->set('user_id', 91);
         return $this->render('VanoFashionEShoppingBundle:Item:index.html.twig');
     }
+
+    
+    public function viewCustomerTypeAction($customerType, $catalogType, $_locale, $page, Request $request)
+    {
+
+    }
+
+
+    public function viewProductAction($customerType, $product , $catalogType, $_locale, $page, Request $request)
+    {
+
+    }
+
+    public function viewCategoryAction($customerType, $product ,$category, $catalogType, $_locale, $page, Request $request)
+    {
+
+    }
+
+    public function viewAction($_locale, $id, Request $request)
+    {
+
+    }
+
+
+    public function managementListItemViewAction($_locale, $page, Request $request)
+    {
+
+    }
+
+
+
+    public function addAction($_locale)
+    {
+
+    }
+
+    public function editAction($_locale, $id)
+    {
+
+    }
+
+    public function deleteAction($_locale, $id)
+    {
+
+    }
+
+
+
+
 }
