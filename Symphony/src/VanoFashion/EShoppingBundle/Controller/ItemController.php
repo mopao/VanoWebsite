86d1d434e575f7d15c92fa5239d5e39131695bfd
mopao <franckyves78@yahoo.fr>
2017-08-01 11:35:07 -0400

    controleur definition

--- Symphony/src/VanoFashion/EShoppingBundle/Controller/ItemController.php
+++ Symphony/src/VanoFashion/EShoppingBundle/Controller/ItemController.php
@@ -17,46 +17,76 @@ class ItemController extends Controller
         return $this->render('VanoFashionEShoppingBundle:Item:index.html.twig');
     }
 
-    
+    /**
+     *return the view of items  by customerType
+     *
+     */
     public function viewCustomerTypeAction($customerType, $catalogType, $_locale, $page, Request $request)
     {
 
     }
 
-
+    /**
+     *return the view of items  by product
+     *
+     */
     public function viewProductAction($customerType, $product , $catalogType, $_locale, $page, Request $request)
     {
 
     }
+
+    /**
+     *return the view of items  by category
+     *
+     */
 
     public function viewCategoryAction($customerType, $product ,$category, $catalogType, $_locale, $page, Request $request)
     {
 
     }
 
+    /**
+     *return the view of item
+     *
+     */
+
     public function viewAction($_locale, $id, Request $request)
     {
 
     }
 
-
+    /**
+     *return the management view of items  
+     *
+     */
     public function managementListItemViewAction($_locale, $page, Request $request)
     {
 
     }
 
 
-
+    /**
+     *return the view for the adding of item
+     *
+     */
     public function addAction($_locale)
     {
 
     }
-
+    
+    /**
+     *return the view for the editing of item
+     *
+     */
     public function editAction($_locale, $id)
     {
 
     }
-
+    
+    /**
+     * delete an item
+     *
+     */
     public function deleteAction($_locale, $id)
     {
 
